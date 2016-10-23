deaef24608559c5290f6dbfe054677655cdf62a2
byyMac <471073043@qq.com>
2016-10-23 21:36:37 +0800

    add api

--- HierarchyMap.d.ts
+++ HierarchyMap.d.ts
@@ -1,9 +1,12 @@
 declare module 'hierarchy-map' {
     var HierarchyMap: {
         new(arr: Array<string|Array<string>>): HierarchyMap
+        count: number
         root: symbol
     }
     interface HierarchyMap {
+        freeze():void
+        ancestorsArray(type: string): Array<string|symbol>
         isap(type1: string, type2: string|symbol): boolean
         root: symbol
     }
